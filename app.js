c47a7dfd804b659a2e8bca68b06b0c0f2a2c5ee2
Tunarrr13 <tunarmemmedli152@gmail.com>
2025-04-10 20:12:09 +0400

    finish

---
+++ app.js
@@ -0,0 +1,9 @@
+let eded =prompt("Bir eded daxil edin");
+
+if (eded > 0) {
+    console.log("Musbet eded");
+} else if (eded < 0) {
+    console.log("Menfi eded");
+} else {
+    console.log("Ədəd sifirdir");
+}
---
+++ app2.js
@@ -0,0 +1,9 @@
+let reqem = prompt("bir reqem daxil edin");
+
+if (reqem < 10) {
+    console.log("10 dan kicikdir");
+} else if (reqem == 10) {
+    console.log("10 a beraberdir");
+} else {
+    console.log("10dan boyukdur");
+}
